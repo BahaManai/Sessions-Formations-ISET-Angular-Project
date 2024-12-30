9242ab991a76190424f2c4be93f65bf8ad486b42
Baha Manai <70824369+BahaManai@users.noreply.github.com>
2024-12-30 14:56:03 +0100

    Création des modèles

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PublicRoutingModule } from './public-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    PublicRoutingModule
   ]
 })
-export class AdminModule { }
+export class PublicModule { }
