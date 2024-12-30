eb79ce9183461d73b3169371e86cae7e9daef1e4
Baha Manai <70824369+BahaManai@users.noreply.github.com>
2024-12-31 00:56:49 +0100

    accueil part1 + Reactive programming + enhance navbar

--- src/app/Services/formation-service.service.ts
+++ src/app/Services/formation-service.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Formation } from '../Models/formation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormationServiceService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  private baseURL: string = 'http://localhost:3000/formations';
+  private options = {
+    headers : new HttpHeaders(
+    {
+      'content-type' : "application/json"
+    }
+    )
+  }
+  
+  getFormations(): Observable<Formation[]> {
+    return this.httpClient.get<Formation[]>(this.baseURL);
+  }
 }
--- src/app/public/accueil/accueil.component.ts
+++ src/app/public/accueil/accueil.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Formation } from '../../Models/formation';
+import { FormationServiceService } from '../../Services/formation-service.service';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit{
+  formations: Formation[] = [];
+  categories: string[] = [];
 
+  constructor(private formationService: FormationServiceService) { }
+  ngOnInit(): void {
+    this.formationService.getFormations().subscribe({
+      next: (res) => {
+        this.formations = res;
+        this.formations.map(formation => {
+          formation.categories.map(category => {
+            if (!this.categories.includes(category)) {
+              this.categories.push(category);
+            }
+          });
+        });
+      }
+    })
+  }
 }
